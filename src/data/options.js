fda00ef69f30e1246939fb7caf06c966b74557c0
Raul Wierzbinski <Wierzba@tuta.io>
2023-10-04 19:38:25 +0200

    Edit .gitignore

--- src/data/options.js
+++ src/data/options.js
@@ -4,7 +4,7 @@ const options = {
     method: 'GET',
     url: 'https://weatherapi-com.p.rapidapi.com/current.json',
     headers: {
-      'X-RapidAPI-Key': 'c93fe7c385msh872650a3afa5646p1ea118jsn32911bbfd00f',
+      'X-RapidAPI-Key': '<API_KEY>',
       'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
     }
 };
